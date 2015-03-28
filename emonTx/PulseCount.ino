733c9bb0f5d0cdfc355046b28c1bb44df9491342
Stephen Fewings <sfeiwngs@iinet.net.au>
2015-03-28 08:53:42 +0800

    EmonTx: Add usage of EmonShared. Clean up sources

--- emonTx/PulseCount.ino
+++ emonTx/PulseCount.ino
@@ -9,109 +9,107 @@
 #define RAIN_GAUGE_PIN		5
 
 // interrupt driven pulse counts 
-#define POWER_PULSE_PIN     3   
-#define INTERRUPT_IR        1    // ATmega 168 and 328 - interrupt 0 = pin 2, 1 = pin 3
+#define POWER_PULSE_PIN	 3	
+#define INTERRUPT_IR		1	// ATmega 168 and 328 - interrupt 0 = pin 2, 1 = pin 3
 
-volatile unsigned short   g_wHrCount = 0;
-volatile unsigned long    g_lastTick = 0;
-volatile unsigned long    g_period   = 0;
+volatile unsigned short	g_wHrCount = 0;
+volatile unsigned long	g_lastTick = 0;
+volatile unsigned long	g_period	= 0;
 
-volatile unsigned short   g_RGCount = 0;
-volatile unsigned long    g_RGlastTick = 0;
+volatile unsigned short	g_RGCount = 0;
+volatile unsigned long	g_RGlastTick = 0;
 
 
 unsigned short MeterPulseLog()
 {
-    unsigned short wattSensorCountIR;  //number of watts during this logging interval 
-    //uint8_t oldSREG = SREG;          // save interrupt register
-    //cli();                           // prevent interrupts while accessing the count   
-    wattSensorCountIR = g_wHrCount;	   //get the count from the interrupt handler 
-    g_wHrCount = 0;                    //reset the watts count
-    //SREG = oldSREG;                  // restore interrupts
+	unsigned short wattSensorCountIR;	// number of watts during this logging interval 
+	uint8_t oldSREG = SREG;						// save interrupt register
+	cli();														// prevent interrupts while accessing the count	
+	wattSensorCountIR = g_wHrCount;		// get the count from the interrupt handler 
+	g_wHrCount = 0;										// reset the watts count
+	SREG = oldSREG;										// restore interrupts
 
-    return wattSensorCountIR;
+	return wattSensorCountIR;
 }
 
 unsigned short MeterRainGauge()
 {
-    return g_RGCount;
+	return g_RGCount;
 }
 
 
 unsigned short MeterCurrentWatts()
-{  
-  unsigned long lastPeriod; 
-  unsigned long thisPeriod; 
-  unsigned long lastTick;
-  unsigned long tick = millis();
-  double watts;
-  
-  uint8_t oldSREG = SREG;          // save interrupt register
-  cli();                           // prevent interrupts while accessing the count   
-  
-  lastPeriod = g_period;
-  lastTick = g_lastTick;
-  
-  SREG = oldSREG;                  // restore interrupts
+{	
+	unsigned long lastPeriod; 
+	unsigned long thisPeriod; 
+	unsigned long lastTick;
+	unsigned long tick = millis();
+	double watts;
+	
+	uint8_t oldSREG = SREG;			// save interrupt register
+	cli();							// prevent interrupts while accessing the count	
+	
+	lastPeriod = g_period;
+	lastTick = g_lastTick;
+	
+	SREG = oldSREG;					// restore interrupts
 
-  if( tick < lastTick )
-    thisPeriod = tick + (lastTick-0xFFFFFFFF);  //rollover occured
-  else
-    thisPeriod = tick - lastTick;
+	if( tick < lastTick )
+		thisPeriod = tick + (lastTick-0xFFFFFFFF);	//rollover occured
+	else
+		thisPeriod = tick - lastTick;
 
-  if( thisPeriod < lastPeriod )
-    watts = ((double)1000)*3600/lastPeriod;       //report the watts for the last tick period
-  else if( thisPeriod >120000 )                   
-    watts = 0;                                    // 2 minutes. That is less than 30 watts/hour. report 0 instead of tappering off slowly towards 0 over time.
-  else
-    watts = ((double)1000)*3600/thisPeriod;       //report the wattage as if the tick occured now 
-  
-  return (unsigned short) watts;
+	if( thisPeriod < lastPeriod )
+		watts = ((double)1000)*3600/lastPeriod;		//report the watts for the last tick period
+	else if( thisPeriod >120000 )					
+		watts = 0;									// 2 minutes. That is less than 30 watts/hour. report 0 instead of tappering off slowly towards 0 over time.
+	else
+		watts = ((double)1000)*3600/thisPeriod;		//report the wattage as if the tick occured now 
+	
+	return (unsigned short) watts;
 }
 
 void SetupPulseCount()
 {
-  g_lastTick = millis();
+	g_lastTick = millis();
 
-  //set up the interrupt handler that will count the IR LED watt pulse counts
-  pinMode(POWER_PULSE_PIN, INPUT);     
-  digitalWrite(POWER_PULSE_PIN, HIGH);   
-  attachInterrupt(INTERRUPT_IR, interruptHandlerIR, FALLING);   
+	//set up the interrupt handler that will count the IR LED watt pulse counts
+	pinMode(POWER_PULSE_PIN, INPUT);	 
+	digitalWrite(POWER_PULSE_PIN, HIGH);	
+	attachInterrupt(INTERRUPT_IR, interruptHandlerIR, FALLING);	
 
-
-  pinMode(RAIN_GAUGE_PIN, INPUT_PULLUP);
-  attachPinChangeInterrupt(RAIN_GAUGE_PIN, interruptHandlerRainGauge, RISING);
-
+	pinMode(RAIN_GAUGE_PIN, INPUT_PULLUP);
+	attachPinChangeInterrupt(RAIN_GAUGE_PIN, interruptHandlerRainGauge, RISING);
 }
 
 
 void interruptHandlerIR() // routine called when external interrupt is triggered
 {
-  //digitalWrite(LEDpin, HIGH);     //flash LED - very quickly each time a pluse occus  
-  
-  g_wHrCount = g_wHrCount + 1;  //Update number of pulses, 1 pulse = 1 watt
-  unsigned long tick = millis();
-  if( tick < g_lastTick )
-    g_period = tick + (g_lastTick-0xFFFFFFFF);  //rollover occured
-  else
-    g_period = tick - g_lastTick;
-  g_lastTick = tick;
+	//digitalWrite(LEDpin, HIGH);	 //flash LED - very quickly each time a pluse occus	
+	
+	g_wHrCount = g_wHrCount + 1;	//Update number of pulses, 1 pulse = 1 watt
+	unsigned long tick = millis();
+	if( tick < g_lastTick )
+		g_period = tick + (g_lastTick-0xFFFFFFFF);	//rollover occured
+	else
+		g_period = tick - g_lastTick;
+	g_lastTick = tick;
 
-  //digitalWrite(LEDpin, LOW);     //flash LED - very quickly each time a pluse occus  
+	//digitalWrite(LEDpin, LOW);	 //flash LED - very quickly each time a pluse occus	
 }
 
 
 // Rain gauge interrupt routine
 void interruptHandlerRainGauge() 
 {
-  unsigned long tick = millis();
-  unsigned long period;
-  if( tick < g_RGlastTick )
-    period = tick + (g_RGlastTick-0xFFFFFFFF);  //rollover occured
-  else
-    period = tick - g_RGlastTick;
-  g_RGlastTick = tick;
+	unsigned long tick = millis();
+	unsigned long period;
+	if( tick < g_RGlastTick )
+		period = tick + (g_RGlastTick-0xFFFFFFFF);	//rollover occured
+	else
+		period = tick - g_RGlastTick;
+	g_RGlastTick = tick;
 
-  if( period > 500 )  //more than 500 ms to avoid switch bounce
-    g_RGCount++;  //Update number of pulses, 1 pulse = 0.2mm of rain
+	if( period > 500 )	//more than 500 ms to avoid switch bounce
+		g_RGCount++;	//Update number of pulses, 1 pulse = 0.2mm of rain
 }
--- emonTx/emonTx.ino
+++ emonTx/emonTx.ino
@@ -1,11 +1,3 @@
-/*
-                          _____    
-                         |_   _|   
-  ___ _ __ ___   ___  _ __ | |_  __
- / _ \ '_ ` _ \ / _ \| '_ \| \ \/ /
-|  __/ | | | | | (_) | | | | |>  < 
- \___|_| |_| |_|\___/|_| |_\_/_/\_\
- 
 //--------------------------------------------------------------------------------------
 //Interrupt pulse counting and one CT sensor 
 //not low power - run emonTx from 5V USB adapter 
@@ -16,7 +8,7 @@
 // openenergymonitor.org
 // GNU GPL V3
 
-//  Modified by S Fewings.
+//	Modified by S Fewings.
 //		1. Use power calculations from home power monitor project
 //		2. Add temperature reading
 //		3. Add rain gauge pulse counting. Added to Arduino IO pin 5, Jeenode Port 2. Une PinChangeInt library to add additional interrupt to Atmega328
@@ -27,13 +19,14 @@
 // CT input on CT channel 2 (middle jack on RHS)
 
 //--------------------------------------------------------------------------------------
-*/
 
 //JeeLabs libraries 
 #include <Ports.h>
 #include <RF12.h>
 #include <avr/eeprom.h>
-#include <util/crc16.h>  //cyclic redundancy check
+#include <util/crc16.h>		//cyclic redundancy check
+#include <time.h>					//required for EmonShared.h
+#include <EmonShared.h>
 
 //---------------------------------------------------------------------------------------------------
 // Serial print settings - disable all serial prints if SERIAL 0 - increases long term stability 
@@ -44,208 +37,146 @@
 //---------------------------------------------------------------------------------------------------
 // RF12 settings 
 //---------------------------------------------------------------------------------------------------
-// fixed RF12 settings
-
-#define myNodeID 10         //in the range 1-30
-#define network     210      //default network group (can be in the range 1-250). All nodes required to communicate together must be on the same network group
-#define freq RF12_915MHZ     //Frequency of RF12B module can be RF12_433MHZ, RF12_868MHZ or RF12_915MHZ. You should use the one matching the module you have.
-
-// set the sync mode to 2 if the fuses are still the Arduino default
-// mode 3 (full powerdown) can only be used with 258 CK startup fuses
-#define RADIO_SYNC_MODE 2
-
-#define COLLECT 0x20 // collect mode, i.e. pass incoming without sending acks
-//--------------------------------------------------------------------------------------------------
-
-
-//---------------------------------------------------------------------------------------------------
-// Pulse counting settings 
-//---------------------------------------------------------------------------------------------------
-
-//long pulseCount = 0;               //Number of pulses, used to measure energy.
-//unsigned long pulseTime,lastTime;  //Used to measure power.
-//double power, elapsedWh;          //power and energy
-//int ppwh = 1;                    ////1000 pulses/kwh = 1 pulse per wh - Number of pulses per wh - found or set on the meter.
-
-//---------------------------------------------------------------------------------------------------
+RF12Init rf12Init = { EMON_NODE, RF12_915MHZ, 210 };
 
 //--------------------------------------------------------------------------------------------------
 // CT energy monitor setup definitions 
 //--------------------------------------------------------------------------------------------------
-int CT_INPUT_PIN =          0;    //I/O analogue 3 = emonTx CT2 channel. Change to analogue 0 for emonTx CT1 chnnel  
-int NUMBER_OF_SAMPLES =     1480; //The period (one wavelength) of mains 50Hz is 20ms. Each samples was measured to take 0.188ms. This meas that 106.4 samples/wavelength are possible. 1480 samples takes 280.14ms which is 14 wavelengths. 
-int RMS_VOLTAGE =           240;  //Assumed supply voltage (230V in UK).  Tolerance: +10%-6%
-int CT_BURDEN_RESISTOR =    15;   //value in ohms of burden resistor R3 and R6
-int CT_TURNS =              1500; //number of turns in CT sensor. 1500 is the vaue of the efergy CT 
+int CT_INPUT_PIN =			0;	//I/O analogue 3 = emonTx CT2 channel. Change to analogue 0 for emonTx CT1 chnnel	
+int NUMBER_OF_SAMPLES =	 1480; //The period (one wavelength) of mains 50Hz is 20ms. Each samples was measured to take 0.188ms. This meas that 106.4 samples/wavelength are possible. 1480 samples takes 280.14ms which is 14 wavelengths. 
+int RMS_VOLTAGE =			240;	//Assumed supply voltage (230V in UK).	Tolerance: +10%-6%
+int CT_BURDEN_RESISTOR =	15;	//value in ohms of burden resistor R3 and R6
+int CT_TURNS =				1500; //number of turns in CT sensor. 1500 is the vaue of the efergy CT 
 
-double CAL=(1.295000139 *1.6);          //*calibration coefficient* IMPORTANT - each monitor must be calibrated for maximum accuracy. See step 4 http://openenergymonitor.org/emon/node/58. Set to 1.295 for Seedstudio 100A current output CT (included in emonTx V2.0 kit)
+double CAL=(1.295000139 *1.6);			//*calibration coefficient* IMPORTANT - each monitor must be calibrated for maximum accuracy. See step 4 http://openenergymonitor.org/emon/node/58. Set to 1.295 for Seedstudio 100A current output CT (included in emonTx V2.0 kit)
 //--------------------------------------------------------------------------------------------------
 
 
 //--------------------------------------------------------------------------------------------------
-// LED Indicator  
+// LED Indicator	
 //--------------------------------------------------------------------------------------------------
-# define LEDpin 9          //hardwired on emonTx PCB
+# define LEDpin 9			//hardwired on emonTx PCB
+
 //--------------------------------------------------------------------------------------------------
-
-
 //Temperature support - OneWire Dallas temperature sensor
-
+//--------------------------------------------------------------------------------------------------
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS 4                                                  // Data wire is plugged into port 2 on the Arduino
-OneWire oneWire(ONE_WIRE_BUS);                                          // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-DallasTemperature sensors(&oneWire);                                    // Pass our oneWire reference to Dallas Temperature.
+#define ONE_WIRE_BUS 4													// Data wire is plugged into port 2 on the Arduino
+OneWire oneWire(ONE_WIRE_BUS);											// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+DallasTemperature sensors(&oneWire);									// Pass our oneWire reference to Dallas Temperature.
 
-// By using direct addressing its possible to make sure that as you add temperature sensors
-// the temperature sensor to variable mapping will not change.
-// To find the addresses of your temperature sensors use the: **temperature_search sketch**
-// DeviceAddress address_T1 = { 0x28, 0x22, 0x70, 0xEE, 0x02, 0x00, 0x00, 0xB8 };
 
-//int supplyV; //no need to send supply voltage as emonTx will be powered from USB adapter when using pulse counting 
-//########################################################################################################################
-//Data Structure to be sent
-//######################################################################################################################## 
-typedef struct {
-  	  int power;		// power value
-          int pulse;            //pulse increments 
-          int ct1;              //CT reading 
-          int supplyV;          // unit supply voltage
-		  int temperature;		//DB1820 temperature
-		  int rainGauge;		//rain gauge pulse
-} Payload;
-Payload emontx;
+
+
+PayloadEmon emonPayload;
 //########################################################################################################################
 
 
 
 void setup()
 {
-  Serial.begin(9600);                   //fast serial 
-  pinMode(LEDpin, OUTPUT);
-  digitalWrite(LEDpin, HIGH);    //turn on LED 
+	Serial.begin(9600);					//fast serial 
+	pinMode(LEDpin, OUTPUT);
+	digitalWrite(LEDpin, HIGH);	//turn on LED 
 
-  delay(1000);
-  
-  Serial.println(F("Fewings emonTx. Based on openenergymonitor.org"));
-  
-  delay(10);  
-  //-----------------------------------------
-  // RFM12B Initialize
-  //------------------------------------------
-  rf12_initialize(myNodeID,freq,network,1600);     //Initialize RFM12 with settings defined above, use pin 10 as SSelect
-  //------------------------------------------
-  
-  delay(20);
-  
-  Serial.print(F("Node: ")); 
-  Serial.print(myNodeID); 
-  Serial.print(F(" Freq: ")); 
-  if (freq == RF12_433MHZ) Serial.print(F("433Mhz"));
-  if (freq == RF12_868MHZ) Serial.print(F("868Mhz"));
-  if (freq == RF12_915MHZ) Serial.print(F("915Mhz"));  
-  Serial.print(F(" Network: ")); 
-  Serial.println(network);
-  delay(20);
-  
-  if (SERIAL==0) {
-    Serial.println("serial disabled"); 
-    Serial.end();
-  }
+	delay(1000);
+	
+	Serial.println(F("Fewings emonTx. Based on openenergymonitor.org"));
+	
+	delay(10);	
+	//-----------------------------------------
+	// RFM12B Initialize
+	//------------------------------------------
+	rf12_initialize(rf12Init.node, rf12Init.freq, rf12Init.group,1600);	 //Initialize RFM12 with settings defined above, use pin 10 as SSelect
+	EmonSerial::PrintRF12Init(rf12Init);
+	//------------------------------------------
+	
+	delay(20);
+	
+	if (SERIAL==0) {
+	Serial.println("serial disabled"); 
+	Serial.end();
+	}
 
-  SetupPulseCount();
+	SetupPulseCount();
 
 
-  //temperature
-  sensors.begin();
+	//temperature
+	sensors.begin();
 
-  DeviceAddress tmp_address;
-  int numberOfDevices = sensors.getDeviceCount();
-  
-  for(int i=0;i<numberOfDevices; i++)
-  {
-    sensors.getAddress(tmp_address, i);
-	Serial.print(F("Temperature sensor address "));
-	Serial.print(i+1);
-	Serial.print(F(": "));
-    printAddress(tmp_address);
-    Serial.println();
-  }
+	DeviceAddress tmp_address;
+	int numberOfDevices = sensors.getDeviceCount();
+	
+	for(int i=0;i<numberOfDevices; i++)
+	{
+		sensors.getAddress(tmp_address, i);
+		Serial.print(F("Temperature sensor address "));
+		Serial.print(i+1);
+		Serial.print(F(": "));
+		printAddress(tmp_address);
+		Serial.println();
+	}
 
-  // Initialise 
-  emontx.pulse = 0;      
-  emontx.ct1 = 0;        
-  emontx.supplyV = 0;    
-  emontx.temperature = 0;
-  emontx.rainGauge = 0;	
+	// Initialise 
+	emonPayload.pulse = 0;		
+	emonPayload.ct1 = 0;
+	emonPayload.supplyV = 0;
+	emonPayload.temperature = 0;
+	emonPayload.rainGauge = 0;
 
-  Serial.println(F("emontx: Power IR_Pulse CurrentTransformer Temperature RainGauge"));
+	EmonSerial::PrintEmonPayload(NULL);
 
-  digitalWrite(LEDpin, LOW);              //turn off LED
+	digitalWrite(LEDpin, LOW);				//turn off LED
 }
 
 
 void loop()
-{  
-   //--------------------------------------------------------------------------------------------------
-    // 1. Read current supply voltage and get current CT energy monitoring reading 
-    //--------------------------------------------------------------------------------------------------
-    emontx.power = MeterCurrentWatts(); 
-    emontx.pulse = MeterPulseLog();					//as well as sending power we also send pulse increments inorder to accuratly calculate kwhr
-    emontx.supplyV = readVcc();					//read emontx supply voltage
-    emontx.ct1= int(emon( CT_INPUT_PIN, CAL, RMS_VOLTAGE, NUMBER_OF_SAMPLES, CT_BURDEN_RESISTOR, CT_TURNS, emontx.supplyV));
-	emontx.rainGauge = MeterRainGauge();			//return the raingauge counts. 
-	sensors.requestTemperatures();  
-	emontx.temperature = sensors.getTempCByIndex(0)*100;
+{	
+	//--------------------------------------------------------------------------------------------------
+	// 1. Read current supply voltage and get current CT energy monitoring reading 
+	//--------------------------------------------------------------------------------------------------
+	emonPayload.power = MeterCurrentWatts();
+	emonPayload.pulse = MeterPulseLog();					//as well as sending power we also send pulse increments inorder to accuratly calculate kwhr
+	emonPayload.supplyV = readVcc();					//read emonPayload supply voltage
+	emonPayload.ct1 = int(emon(CT_INPUT_PIN, CAL, RMS_VOLTAGE, NUMBER_OF_SAMPLES, CT_BURDEN_RESISTOR, CT_TURNS, emonPayload.supplyV));
+	emonPayload.rainGauge = MeterRainGauge();			//return the raingauge counts. 
+	sensors.requestTemperatures();	
+	emonPayload.temperature = sensors.getTempCByIndex(0) * 100;
 
-    //--------------------------------------------------------------------------------------------------
-    // 2. Send data via RF 
-    //--------------------------------------------------------------------------------------------------
-    while (!rf12_canSend())
-    rf12_recvDone();
-    rf12_sendStart(0,&emontx, sizeof emontx); 
-    //rf12_sendStart(rf12_hdr, &emontx, sizeof emontx, RADIO_SYNC_MODE); 
-    //--------------------------------------------------------------------------------------------------    
+	//--------------------------------------------------------------------------------------------------
+	// 2. Send data via RF 
+	//--------------------------------------------------------------------------------------------------
+	while (!rf12_canSend())
+	rf12_recvDone();
+	rf12_sendStart(0, &emonPayload, sizeof emonPayload);
+	//--------------------------------------------------------------------------------------------------	
+	EmonSerial::PrintEmonPayload(&emonPayload);
 
 
-    if (SERIAL==1){
-      Serial.print(F("emontx: "));
-      Serial.print(emontx.power);
-      Serial.print(F(" "));
-      Serial.print(emontx.pulse);
-      Serial.print(F(" "));
-      Serial.print(emontx.ct1);
-      Serial.print(F(" "));
-      //Serial.print(emontx.supplyV);
-      //Serial.print(" ");
-      Serial.print(emontx.temperature);
-      Serial.print(F(" "));
-	  Serial.println(emontx.rainGauge);
-    }
-   
-    digitalWrite(LEDpin, HIGH);     //flash LED - very quickly each time a pluse occus  
-    delay(1);
-	digitalWrite(LEDpin, LOW);     //flash LED - very quickly each time a pluse occus  
+	digitalWrite(LEDpin, HIGH);	 //flash LED - very quickly each time a pluse occus	
+	delay(1);
+	digitalWrite(LEDpin, LOW);	 //flash LED - very quickly each time a pluse occus	
 
-    delay(1000);        //1s delay  
+	delay(1000);		//1s delay	
 }
 
 
 
 void printAddress(uint8_t deviceAddress[8])
 {
-  Serial.print("{ ");
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    // zero pad the address if necessary
-    Serial.print("0x");
-    if (deviceAddress[i] < 16) Serial.print("0");
-    Serial.print(deviceAddress[i], HEX);
-    if (i<7) Serial.print(", ");
-    
-  }
-  Serial.print(" }");
+	Serial.print("{ ");
+	for (uint8_t i = 0; i < 8; i++)
+	{
+	// zero pad the address if necessary
+	Serial.print("0x");
+	if (deviceAddress[i] < 16) Serial.print("0");
+	Serial.print(deviceAddress[i], HEX);
+	if (i<7) Serial.print(", ");
+	
+	}
+	Serial.print(" }");
 }
 
 
@@ -253,15 +184,15 @@ void printAddress(uint8_t deviceAddress[8])
 // Read current emonTx battery voltage - not main supplyV!
 //--------------------------------------------------------------------------------------------------
 long readVcc() {
-  long result;
-  // Read 1.1V reference against AVcc
-  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  delay(2); // Wait for Vref to settle
-  ADCSRA |= _BV(ADSC); // Convert
-  while (bit_is_set(ADCSRA,ADSC));
-  result = ADCL;
-  result |= ADCH<<8;
-  result = 1126400L / result; // Back-calculate AVcc in mV
-  return result;
+	long result;
+	// Read 1.1V reference against AVcc
+	ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+	delay(2); // Wait for Vref to settle
+	ADCSRA |= _BV(ADSC); // Convert
+	while (bit_is_set(ADCSRA,ADSC));
+	result = ADCL;
+	result |= ADCH<<8;
+	result = 1126400L / result; // Back-calculate AVcc in mV
+	return result;
 }
 //--------------------------------------------------------------------------------------------------
