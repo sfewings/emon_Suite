f864bd70b0f1d7c460487898f0fb12e7f665412f
Stephen Fewings <sfewings@iinet.net.au>
2019-09-09 14:38:03 +0800

    emon_WaterMeter: Added hall effect sensor support. Installed on fron mains water meter

--- emon_WaterMeter/emon_WaterMeter.ino
+++ emon_WaterMeter/emon_WaterMeter.ino
@@ -11,32 +11,29 @@
 
 #define GREEN_LED 9			// Green LED on emonTx
 bool g_toggleLED = false;
-
-#define ENABLE_LCD 0
-#if ENABLE_LCD
-#include <PortsLCD.h>
-
-LiquidCrystal lcd(A2, 4, 8, 7, 6, 5);
-
-static void lcdSerialReading(uint32_t reading)
-{
-	lcd.setCursor(0, 1);
-	for (int i = 3; i >= 0; i--)
-	{
-		lcd.print((reading >> (8 * i)) & 0xFF);
-		if (i)
-			lcd.print(",");
-	}
-}
-
-#endif
+bool g_lastActivity = false;
 
 #define EEPROM_BASE 0x10	//where the water count is stored
 
-// If your sensor is connected to Serial, Serial1, Serial2, AltSoftSerial, etc. pass that object to the sensor constructor.
-
 #define TIMEOUT_PERIOD 2000		//10 seconds in ms. don't report litres/min if no tick recieved in this period
 #define PULSES_PER_LITRE  1000
+#define HALL_PEAK_THRESHOLD 3	//the minimum change in reading required to signal a change from rising to falling
+
+enum calibrationStep { eStart, eWaitingForPeak, eFirstPass, eSecondPass, eCalculate, eFinished };
+
+typedef struct HallEffectStateType
+{
+	int calibrationStep;
+	bool rising;
+	int maxValue, minValue;
+	int maxCalibration, minCalibration;
+	int lastValue;
+	int lValue[10];
+	int lastSector;
+} HallEffectStateType;
+
+HallEffectStateType g_hallEffectState;
+
 
 volatile unsigned long	g_flowCount = 0;		//number of pulses in total since installation
 volatile unsigned long	g_lastTick = 0; 		//millis() value at last pulse
@@ -44,7 +41,10 @@ volatile unsigned long	g_period = 0;				//ms between last two pulses
 
 RF12Init rf12Init = { WATERLEVEL_NODE, RF12_915MHZ, FEWINGS_MONITOR_GROUP, RF69_COMPAT };
 
-PayloadWater waterPayload;
+PayloadWater g_waterPayload;
+
+ISR(WDT_vect) { Sleepy::watchdogEvent(); }
+
 
 unsigned long readEEPROM(int offset)
 {
@@ -120,36 +120,234 @@ unsigned short FlowRateInLitresPerMinute()
 }
 
 
+int ServiceHallEffectSensor(struct HallEffectStateType &state)
+{
+	int litresReturn = 0;
+	int value = analogRead(A0);
+
+	//Calibration process
+	if (state.calibrationStep != eFinished)
+	{
+		Serial.print("Calibrating step(");
+		Serial.print(state.calibrationStep);
+		Serial.print(") ");
+		Serial.print((state.rising?"^":"|"));
+
+		switch (state.calibrationStep)
+		{
+		case eStart:
+			state.minCalibration = value;
+			state.maxCalibration = value;
+			if (value < state.lastValue)
+			{
+				state.rising = false;
+				state.calibrationStep = eWaitingForPeak;
+			}
+			else if (value > state.lastValue)
+			{
+				state.rising = true;
+				state.calibrationStep = eWaitingForPeak;
+			}
+			break;
+		case eWaitingForPeak:
+		case eFirstPass:
+		case eSecondPass:
+			if (state.rising)
+			{
+				if (value > state.maxCalibration)
+				{
+					state.maxCalibration = value;
+				}
+				else if (value < state.lastValue && fabs(value - state.maxCalibration) > HALL_PEAK_THRESHOLD)  //be sure that the fall is more than 5/1024 of range to remove noise at top of readings
+				{
+					state.rising = false;
+					state.calibrationStep++;
+				}
+			}
+			else //falling
+			{
+				if (value < state.minCalibration)
+				{
+					state.minCalibration = value;
+				}
+				else if (value > state.lastValue && fabs(value - state.minCalibration) > HALL_PEAK_THRESHOLD)
+				{
+					state.rising = true;
+					state.calibrationStep++;
+				}
+			}
+			break;
+		case eCalculate:
+			{
+				if (state.maxCalibration - state.minCalibration > 40) //40 of range 0 - 1024 (100/1024 * 5v)
+				{
+					Serial.println("Calibration values: ");
+					Serial.print(" minCalibration=");
+					Serial.print(state.minCalibration);
+					Serial.print(" maxCalibration=");
+					Serial.println(state.maxCalibration);
+					for (int i = 0; i < 10; i++)
+					{
+						state.lValue[i] = (state.maxCalibration + state.minCalibration)/2 + (state.maxCalibration - state.minCalibration)/2 * sin((-72 + (double)i * 36.0)*PI/180);  //9, 27, 45, 63, 81, 99, 117, 135, 153, 171  degrees
+						//Serial.print((int)(-81 + (double)i * 36.0));
+						Serial.print("(");
+						Serial.print(i);
+						Serial.print(")");
+						Serial.print(state.lValue[i]);
+						Serial.print(", ");
+					}
+					Serial.println();
+					state.calibrationStep = eFinished;
+					state.maxValue = (state.rising? value:0);
+					state.minValue = (state.rising? 1024 : value);
+					state.lastSector = (state.rising ? 0: 5); //set the sector we are in at end of calibration
+				}
+				else
+					state.calibrationStep = eStart;		//start calibration process again!
+				litresReturn = 12;		//Somewhere between 10 and 15 litres should have passed during the calibration process
+				break;
+			}
+		}
+		Serial.print((state.rising ? "^" : "|"));
+		state.lastValue = value;
+	}
+	else  //monitoring process
+	{
+		int lThisSector = state.lastSector;
+
+		//iterate the 9 sectors past the last reported sector. We don't iterate the 10th sector as we want to avoid a noisy signal moving us 10 sectors ahead
+		//Find out how far we have moved through the cycle
+		for (int i = state.lastSector; i < 9 + state.lastSector; i++)
+		{
+			int sector = i % 10;
+			if (sector == 0 )
+			{
+				if (value <= state.lValue[0])
+				{
+					lThisSector = sector;
+					break;
+				}
+			}
+			else if ( sector == 5)
+			{
+				if (value >= state.lValue[4])
+				{
+					lThisSector = sector;
+					break;
+				}
+			}
+			else if (sector > 0 && sector < 5)
+			{
+				if (value > state.lValue[sector-1] && value <= state.lValue[sector])
+				{
+					lThisSector = sector;
+					break;
+				}
+			}
+			else if (sector > 5 && sector < 10)
+			{
+				if (value <= state.lValue[sector-1] && value >= state.lValue[sector])
+				{
+					lThisSector = sector;
+					break;
+				}
+			}
+		}
+
+		if (lThisSector < state.lastSector)
+		{
+			//passed end of sectors (full 360 degree)
+			litresReturn = 10 + lThisSector - state.lastSector;
+		}
+		else
+		{
+			litresReturn = lThisSector - state.lastSector;
+		}
+
+		if (litresReturn)
+		{
+			Serial.print(state.lastSector);
+			Serial.print(",");
+			Serial.print(lThisSector);
+			Serial.print(",");
+		}
+
+		state.lastSector = lThisSector;
+
+		//work out if we need to recalibrate the sensor. If the min or max is more than 10% of range outside of last calibration values
+		//if( litresReturn)
+		//	Serial.print((state.rising ? "^" : "|"));
+		//if (state.rising)
+		//{
+		//	//update rising/falling state
+		//	if (value >= state.maxValue)
+		//	{
+		//		state.maxValue = value;
+		//	}
+		//	else if (fabs(value - state.maxValue) > HALL_PEAK_THRESHOLD)		//changing from rising to falling
+		//	{
+		//		if (fabs(state.maxValue - state.maxCalibration) > (state.maxCalibration - state.minCalibration) /10)// no longer claibrated
+		//		{
+		//			state.calibrationStep = eStart;
+		//		}
+		//		else
+		//		{
+		//			state.rising = false;
+		//			state.maxValue = 0; //reset
+		//		}
+		//	}
+		//}
+		//else //falling
+		//{
+		//	//update rising/falling state
+		//	if (value <= state.minValue)
+		//	{
+		//		state.minValue = value;
+		//	}
+		//	else if (fabs(value - state.minValue) > HALL_PEAK_THRESHOLD)		//changing from falling to rising
+		//	{
+		//		if (fabs(state.minValue - state.minCalibration) > (state.maxCalibration - state.minCalibration) / 10)// no longer claibrated
+		//		{
+		//			state.calibrationStep = eStart;
+		//		}
+		//		else
+		//		{
+		//			state.rising = true;
+		//			state.minValue = 1024; //reset
+		//		}
+		//	}
+		//}
+		//if (litresReturn)
+		//	Serial.print((state.rising ? "^" : "|"));
+	}
+
+	if (state.calibrationStep != eFinished || litresReturn)
+	{
+		Serial.print(value);
+		Serial.print(": ");
+		Serial.println(litresReturn);
+	}
+
+	return litresReturn;
+}
+
+
 //--------------------------------------------------------------------------------------------
 // Setup
 //--------------------------------------------------------------------------------------------
 void setup()
 {
+	Serial.begin(9600);
+
+	Serial.println(F("Water sensor start - Hall effect on A0"));
+
 	pinMode(GREEN_LED, OUTPUT);
 	digitalWrite(GREEN_LED, HIGH);		//LED has inverted logic. LOW is on, HIGH is off!
 
-	Serial.begin(9600);
-
-#if ENABLE_LCD
-	lcd.begin(16, 2);
-	lcd.setCursor(0, 0);
-
-	lcd.print(F("Water sensor"));
-	lcd.setCursor(0, 1);
-	lcd.print(F("Monitor 3.0"));
-	delay(1000);
-	lcd.clear();
-#endif
-
-	Serial.println(F("Water sensor start"));
 
 	Serial.println("rf12_initialize");
 	rf12_initialize(rf12Init.node, rf12Init.freq, rf12Init.group);
 	EmonSerial::PrintRF12Init(rf12Init);
-
-
-	sensorSerial.begin(9600);     // Sensor transmits its data at 9600 bps.
-	waterHeightSensor.begin();               // Initialise the sensor library.
 
 
 	//initialise the EEPROMSettings for relay and node number
@@ -157,19 +355,24 @@ void setup()
 	EmonEEPROM::ReadEEPROMSettings(eepromSettings);
 	EmonEEPROM::PrintEEPROMSettings(Serial, eepromSettings);
 
-	waterPayload.subnode = eepromSettings.subnode;
-
+	g_waterPayload.subnode = eepromSettings.subnode;
 
 	//water flow rate setup
 	//writeEEPROM(0, 0);					//reset the flash
 	g_flowCount = readEEPROM(0);	//read last reading from flash
 
 	g_period = millis() - TIMEOUT_PERIOD;
-	waterPayload.flowRate = 0;
-	waterPayload.flowCount = g_flowCount;
+	g_waterPayload.flowRate = 0;
+	g_waterPayload.flowCount = g_flowCount;
 
-	pinMode(3, INPUT_PULLUP);
-	attachInterrupt(digitalPinToInterrupt(3), interruptHandlerWaterFlow, CHANGE);
+
+	//Hall effect sensor AH3503
+	pinMode(A0, INPUT);
+	g_hallEffectState.calibrationStep = eStart;
+	g_hallEffectState.lastValue = analogRead(A0);
+
+//	pinMode(3, INPUT_PULLUP);
+//	attachInterrupt(digitalPinToInterrupt(3), interruptHandlerWaterFlow, CHANGE);
 
 	delay(100);
 	
@@ -181,56 +384,67 @@ void setup()
 //--------------------------------------------------------------------------------------------
 void loop () 
 {
-	char s[16];
+	if (g_hallEffectState.calibrationStep != eFinished)
+	{
+		digitalWrite(GREEN_LED, HIGH);
+	}
 
-	unsigned long flowCount = g_flowCount;
-	uint16_t	waterHeight = 
+	int litres = ServiceHallEffectSensor(g_hallEffectState);
+	
+	g_flowCount += litres;
 
-	bool activity = ( waterPayload.flowCount   != flowCount || 
-										waterPayload.waterHeight != waterHeight   );
+	bool activity = ( litres != 0 );
 
 	if(activity)
 	{
-		writeEEPROM(0, flowCount);
+		digitalWrite(GREEN_LED, HIGH);
+		writeEEPROM(0, g_flowCount);
 	}
 
-	waterPayload.flowCount = flowCount;
-	waterPayload.flowRate = FlowRateInLitresPerMinute();
-	waterPayload.waterHeight = waterHeight;
+	g_waterPayload.flowCount = g_flowCount;
+	g_waterPayload.flowRate = 0;// FlowRateInLitresPerMinute();
+	g_waterPayload.waterHeight = 0;
 
-
-#if ENABLE_LCD
-	//toggle a "*" every read
-	static bool toggle = true;
-	lcd.setCursor(15, 1);
-	lcd.print((toggle = !toggle) ? "*" : " ");
-
-	lcd.setCursor(0, 0);
-	lcd.print(s);
-	lcd.setCursor(0, 1);
-	lcdSerialReading(waterHeightSensor.SerialData());
-	activity = true;	//loop every second
-#endif
-
-	rf12_sleep(RF12_WAKEUP);
-
-	int wait = 1000;
-	while (!rf12_canSend() && wait--)
-		rf12_recvDone();
-	if (wait)
+	//don't transmit while calibrating
+	if (g_hallEffectState.calibrationStep == eFinished)
 	{
-		rf12_sendStart(0, &waterPayload, sizeof waterPayload);
-		rf12_sendWait(0);
-		EmonSerial::PrintWaterPayload(&waterPayload);
+		rf12_sleep(RF12_WAKEUP);
+
+		int wait = 1000;
+		while (!rf12_canSend() && wait--)
+			rf12_recvDone();
+		if (wait)
+		{
+			rf12_sendStart(0, &g_waterPayload, sizeof g_waterPayload);
+			rf12_sendWait(0);
+			EmonSerial::PrintWaterPayload(&g_waterPayload);
+		}
+		else
+		{
+			Serial.println(F("RF12 waiting. No packet sent"));
+		}
+		rf12_sleep(RF12_SLEEP);
+	}
+
+	digitalWrite(GREEN_LED, LOW);
+	
+	//see http://www.gammon.com.au/forum/?id=11428
+	while (!(UCSR0A & (1 << UDRE0)))  // Wait for empty transmit buffer
+		UCSR0A |= 1 << TXC0;  // mark transmission not complete
+	while (!(UCSR0A & (1 << TXC0)));   // Wait for the transmission to complete
+
+
+
+	if (g_hallEffectState.calibrationStep != eFinished)
+		Sleepy::loseSomeTime(100);
+	else if (activity || g_lastActivity)
+	{
+		Sleepy::loseSomeTime(2000);
 	}
 	else
 	{
-		Serial.println(F("RF12 waiting. No packet sent"));
+		Sleepy::loseSomeTime(10000);		//10 seconds. If water flows faster than 54l/min then we have a problem! 
 	}
-	rf12_sleep(RF12_SLEEP);
 
-	if (activity)
-		delay(1000);
-	else
-		delay(30000);
+	g_lastActivity = activity;
 }
