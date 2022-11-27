35e2589ccdcdb0ee29e775a63a41dfd500122841
Stephen Fewings <sfewings@iinet.net.au>
2022-11-27 13:07:26 +0800

    emon_WS2812Banner:Add bigger font.

--- emon_WS2812Banner/emon_WS2812Banner.ino
+++ emon_WS2812Banner/emon_WS2812Banner.ino
@@ -13,8 +13,35 @@
 #include <NeoPixelBus.h>
 #include <PinChangeInt.h>
 
+# define NUM_FONTS 3
 
-const byte digits[12][5]={
+const byte digits[NUM_FONTS][10][5]=
+{
+    {   //5*8 font
+                            {0x3E,0x51,0x49,0x45,0x3E}, //0
+                            {0x00,0x01,0x7F,0x21,0x00}, //1
+                            {0x31,0x49,0x45,0x43,0x21}, //2
+                            {0x46,0x69,0x51,0x41,0x42}, //3
+                            {0x04,0x7F,0x24,0x14,0x0C}, //4
+                            {0x4E,0x51,0x51,0x51,0x72}, //5
+                            {0x06,0x49,0x49,0x29,0x1E}, //6
+                            {0x60,0x50,0x48,0x47,0x40}, //7
+                            {0x36,0x49,0x49,0x49,0x36}, //8
+                            {0x3C,0x4A,0x49,0x49,0x30} //9    
+    },
+    {  //5*8 but one pixel higher
+                            {0x7C,0xA2,0x92,0x8A,0x7C}, //0
+                            {0x00,0x02,0xFE,0x42,0x00}, //1
+                            {0x62,0x92,0x8A,0x86,0x42}, //2
+                            {0x8C,0xD2,0xA2,0x82,0x84}, //3
+                            {0x08,0xFE,0x48,0x28,0x18}, //4
+                            {0x9C,0xA2,0xA2,0xA2,0xE4}, //5
+                            {0x0C,0x92,0x92,0x52,0x3C}, //6
+                            {0xC0,0xA0,0x90,0x8E,0x80}, //7
+                            {0x6C,0x92,0x92,0x92,0x6C}, //8
+                            {0x78,0x94,0x92,0x92,0x60} //9        
+    },
+    {   //4*8 font
                     //0
                    {0b00000000, 
                     0b01111100,
@@ -74,20 +101,11 @@ const byte digits[12][5]={
                     0b01111100, 
                     0b10010010, 
                     0b10010010, 
-                    0b01100100},
-                    //on
-                   {0b11111111,
-                    0b11111111, 
-                    0b11111111, 
-                    0b11111111, 
-                    0b11111111},
-                    //off
-                   {0b00000000,
-                    0b00000000, 
-                    0b00000000, 
-                    0b00000000, 
-                    0b00000000},
-                  };
+                    0b01100100}
+                  } 
+};
+
+const uint8_t g_fontWidth[NUM_FONTS] = {5,5,4};
 
 // Singleton instance of the radio driver
 RH_RF69 g_rf69;
@@ -96,20 +114,22 @@ PayloadPulse pulsePayload;
 
 const uint8_t LDR_PIN = A0;
 const uint8_t PIXEL_PIN = 3;
-const uint16_t PixelCount = 256; //512 
+const uint16_t NUM_PIXELS = 256; //512 
 const float MaxLightness = 0.4f; // max lightness at the head of the tail (0.5f is full bright)
+const uint8_t LED_PIN = A3;  //Pin 17
+const uint8_t NUM_BUTTONS = 2;
+const uint8_t  g_buttons[NUM_BUTTONS] = { A1, A2 };	//pin number for each input A1, A2.  Pins 15 & 16
 
-const uint8_t NUM_BUTTONS = 3;
-const uint8_t  g_buttons[NUM_BUTTONS] = { 15, 19, 16 };	//pin number for each input A1, A2, A6
-volatile unsigned long	g_lastButtonPush[NUM_BUTTONS]	= { 0,0,0 };		//millis() value at last pulse
+volatile unsigned long	g_lastButtonPush[NUM_BUTTONS]	= { 0,0 };		//millis() value at last pulse
 
 RgbColor g_colour = RgbColor(255, 255, 255);
-volatile int g_value = 59;
+volatile int g_value = 0;
 
 volatile uint8_t g_buttonPin = 0;
 volatile bool g_invertText = false;
+volatile uint8_t g_fontIndex = 0;
 
-NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount,PIXEL_PIN);
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(NUM_PIXELS,PIXEL_PIN);
 
 
 uint8_t readLDR()
@@ -141,25 +161,32 @@ void interruptHandlerIR()
         if( ++button == NUM_BUTTONS)
             return;		// we only support 3 buttons though we could get an interrupt for more!
     }
+    unsigned long msSinceLastButton = millis()- g_lastButtonPush[button];
     g_lastButtonPush[button] = millis();
+
+    if(msSinceLastButton <20)
+        return;     //button debounce
 
     if( button == 0)
     {
-        g_colour = RgbColor(random(255),random(255),random(255) );
-        g_invertText = !g_invertText;
+        g_fontIndex = ((g_fontIndex+1) % NUM_FONTS);
     }
 
     if( button == 1)
-        g_value++;
-    if( button == 2)
-        g_value--;
+    {
+        g_colour = RgbColor(random(255),random(255),random(255) );
+        if(msSinceLastButton <250)
+            g_invertText = !g_invertText;
+    }
 }
 
 
 
-void bannerDigit(int digit, int offset, uint8_t intensity = 255)
+void bannerDigit(int digit, int offset, RgbColor colour)
 {
-    RgbColor colour = RgbColor(g_colour.R*intensity/255,g_colour.G*intensity/255,g_colour.B*intensity/255);
+    if(digit <0 || digit>9)
+        return;
+
     for(int i=0; i <5;i++)
     {
         if(offset+i >= 0 )
@@ -167,14 +194,13 @@ void bannerDigit(int digit, int offset, uint8_t intensity = 255)
             int index = (offset+i)*8;
             for(int b=0; b<8;b++)
             {
-                RgbColor onColour = g_invertText? colour : RgbColor(0, 0, 0);
-                RgbColor offColour = g_invertText? RgbColor(0, 0, 0) : colour;
+                RgbColor onColour = g_invertText? RgbColor(0, 0, 0) : colour;
+                RgbColor offColour = g_invertText? colour : RgbColor(0, 0, 0);
                 RgbColor pixelColour;
-
                 if( (offset+i) %2 == 0)
-                    pixelColour = digits[digit][i] & 1<<b ? onColour : offColour;
+                    pixelColour = digits[g_fontIndex][digit][i] & 1<<b ? onColour : offColour;
                 else
-                    pixelColour = digits[digit][i] & 1<<(7-b) ? onColour : offColour;
+                    pixelColour = digits[g_fontIndex][digit][i] & 1<<(7-b) ? onColour : offColour;
 
                 strip.SetPixelColor(index+b, pixelColour);
             }
@@ -184,17 +210,22 @@ void bannerDigit(int digit, int offset, uint8_t intensity = 255)
 
 void printValue(int value, int offset = 0, uint8_t intensity = 255)
 {
-    bannerDigit(11, offset, intensity);
-    offset += 5;
+    RgbColor colour = RgbColor(g_colour.R*intensity/255,g_colour.G*intensity/255,g_colour.B*intensity/255);
+
+    for(uint16_t pixel=0; pixel <NUM_PIXELS; pixel++)
+    {
+        strip.SetPixelColor(pixel, (g_invertText?colour:RgbColor(0, 0, 0)));
+    }
+
+    offset += g_fontWidth[g_fontIndex]+1;
 	int places = log10(abs(value));
 	for (int i = 0; i <= places; i++)
     {
         int digit = value % 10;
-		bannerDigit(digit, offset, intensity);
+		bannerDigit(digit, offset, colour);
         value = value/10;
-        offset += 5;
+        offset += g_fontWidth[g_fontIndex]+1;
     }
-    bannerDigit(11, offset, intensity);
     strip.Show();
 }
 
@@ -203,7 +234,7 @@ void testBanner()
 
     for(int digit=0; digit<10;digit++)
     {
-        for(int offset=0;offset<PixelCount/8;offset++)
+        for(int offset=0;offset<NUM_PIXELS/8;offset++)
         {
             bannerDigit( digit, offset, readLDR() );
             strip.Show();
@@ -211,7 +242,7 @@ void testBanner()
         }
     }
 
-    for(int offset = 0; offset<PixelCount/8;offset++)
+    for(int offset = 0; offset<NUM_PIXELS/8;offset++)
     {  bannerDigit( 0, offset+0, readLDR());
         bannerDigit( 1, offset+5, readLDR());
         bannerDigit( 2, offset+10, readLDR());
@@ -241,6 +272,10 @@ void setup()
 	g_rf69.setEncryptionKey(key);
 	g_rf69.setHeaderId(DISPLAY_NODE);
 
+    pinMode( LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
+
+
 	Serial.print("RF69 initialise node: ");
 	Serial.print(DISPLAY_NODE);
 	Serial.println(" Freq: 915MHz");
@@ -259,11 +294,6 @@ void setup()
 
 void loop()
 {
-    // if(millis()-g_lastButtonPush[0] < 100)
-    // {
-        
-    // }
-
 	//--------------------------------------------------------------------------------------------
 	// 1. On RF recieve
 	//--------------------------------------------------------------------------------------------	
@@ -274,6 +304,8 @@ void loop()
 
 	if (g_rf69.available())
 	{
+        digitalWrite(LED_PIN, HIGH);
+        
 		len = RH_RF69_MAX_MESSAGE_LEN;  //ASSERT( len <= sizeof(buf));
 		if (g_rf69.recv(buf, &len))
 		{
@@ -290,12 +322,8 @@ void loop()
 			EmonSerial::PrintPulsePayload(&pulsePayload);				// print data to serial
 		}
     }
+    digitalWrite(LED_PIN, LOW);
 
     printValue((unsigned int) pulsePayload.power[2], 0, readLDR());
-    //printValue(pulsePayload.power[1], 32*8, readLDR());
-    
-    
     delay(10);
-    //printValue(g_value,3, readLDR());
-    //Serial.println(g_value);Serial.print(","); Serial.println(g_buttonPin);
 }
