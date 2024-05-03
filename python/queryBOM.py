eab2e76365d58b4969311a9f704c81285b05235e
Stephen Fewings <sfewings@iinet.net.au>
2024-05-03 16:26:52 +0800

    queryBOM: Restart every 24 hours to overcome 'RuntimeError: can't start new thread'

--- python/queryBOM.py
+++ python/queryBOM.py
@@ -1,6 +1,9 @@
 import sys
 import time
 import argparse
+import socket
+import threading
+
 #from weather_au import api
 from weather_au import observations
 from influxdb_client import InfluxDBClient, Point
@@ -25,7 +28,7 @@ def writeValue(sensorName, temperatureValue, BOM_id, url ):
 #   observations = w.observations()
 #   return observations['temp']
 
-print("Query BOM Perth temperature and publish to influxdb:8086")
+print("Query BOM Perth temperature and publish to influxdb:8086. Version release 18 Apr 2024")
 
 parser = argparse.ArgumentParser(description="Query BOM Perth temperature and publish to influxdb:8086")
 parser.add_argument("-i", "--influx", help= "IP address of influxDB server", default="localhost")
@@ -34,20 +37,34 @@ influxURL = f"http://{args.influx}:8086"
 
 start_time = time.time()
 
+socket.setdefaulttimeout(60)
+
 while 1:
+  #run for 24 hours
+  if time.time() - start_time >= 24*60*60:
+    print("Automatic shutdown after 24 hours to overcome 'RuntimeError: can't start new thread'")
+    break
+  else:
+    elapsed_seconds = time.time() - start_time
+    hours = int(elapsed_seconds // 3600)
+    minutes = int((elapsed_seconds % 3600) // 60)
+    seconds = int(elapsed_seconds % 60)
+    print(f"Running time: {hours:02d}:{minutes:02d}:{seconds:02d}")
+    print(f"threading.active_count() = {threading.active_count()}")
+    
+
 #  temperature = getPerthTemperature()
 #  writeValue("Perth BOM", temperature, 94608, influxURL)
-
-  obs_data = observations.Observations('WA')
-  writeValue("Perth",        float(obs_data.air_temperature(94608)), 94608, influxURL)
-  #writeValue("Meekatharra",  float(obs_data.air_temperature(94430)), 94430, influxURL)
-  #writeValue("Mount Magnet", float(obs_data.air_temperature(94429)), 94429, influxURL)
-  #writeValue("Murchison",    float(obs_data.air_temperature(94422)), 94422, influxURL)
+  try:
+    obs_data = observations.Observations('WA')
+    writeValue("Perth",        float(obs_data.air_temperature(94608)), 94608, influxURL)
+    #writeValue("Meekatharra",  float(obs_data.air_temperature(94430)), 94430, influxURL)
+    #writeValue("Mount Magnet", float(obs_data.air_temperature(94429)), 94429, influxURL)
+    #writeValue("Murchison",    float(obs_data.air_temperature(94422)), 94422, influxURL)
+  except Exception as e:
+      print(f"Exception: {e}")
 
   sys.stdout.flush()
 
   time.sleep(120)
-
-  #run for 24 hours
-  if time.time() - start_time >= 24*60*60:
-    break
+  
