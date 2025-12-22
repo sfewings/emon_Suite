4bf93d5d81e391097aec6cf1a62d6b0f32bf57db
Stephen Fewings <sfewings@iinet.net.au>
2025-12-22 22:25:59 +0800

    emon_Tachometer: All hardware working. Configured for house data. Todo: Increase font size, configure for boat, add EEPROM store of last mode

--- emon_Tachometer/emon_Tachometer.ino
+++ emon_Tachometer/emon_Tachometer.ino
@@ -8,9 +8,43 @@
 #include <PWM.h> 
 #include <Wire.h>
 
+//Note: To stop loading the standard font and saving 1314 bytes of program memory, add the following
+// Adafruit_GFX.cpp
+//  line 31:  #define DONT_USE_STANDARD_FONT
+//  line 1266-1268: surround the line "uint8_t line = pgm_read_byte(&font[c * 5 + i]);" with
+//  #ifdef DONT_USE_STANDARD_FONT
+//       uint8_t line = 255;
+//  #else
+//       uint8_t line = pgm_read_byte(&font[c * 5 + i]);
+//  #endif
+// glcdfont.c add the following lines around the font definition
+//  #ifdef DONT_USE_STANDARD_FONT
+//  extern const unsigned char font[] PROGMEM;
+//  #else
+// <the font definition>
+
 #include <Adafruit_GFX.h>
 #include <gfxfont.h>
 #include <Adafruit_SSD1306.h>
+// Add font headers from the Adafruit GFX "Fonts" folder.
+// If these headers are not present in your installation adjust the paths/names
+// #include <Fonts/FreeMono24pt7b.h>
+// #include <Fonts/FreeSerif24pt7b.h>
+// #include <Fonts/FreeMonoBold24pt7b.h>
+// #include <Fonts/FreeMonoBoldOblique24pt7b.h>
+// #include <Fonts/FreeMonoOblique24pt7b.h>
+// #include <Fonts/FreeSans24pt7b.h>
+// #include <Fonts/FreeSans18pt7b.h>
+#include <Fonts/FreeSans12pt7b.h>
+// #include <Fonts/FreeSansBold24pt7b.h>
+// #include <Fonts/FreeSansBoldOblique24pt7b.h>
+// #include <Fonts/FreeSansOblique24pt7b.h>
+// #include <Fonts/FreeSerif24pt7b.h>
+// #include <Fonts/FreeSerifBold24pt7b.h>
+// #include <Fonts/FreeSerifBoldItalic24pt7b.h>
+// #include <Fonts/FreeSerifItalic24pt7b.h>
+
+
 #include <APDS9930.h>
 #include <RH_RF69.h>
 #include <NeoPixelBus.h>
@@ -26,11 +60,14 @@
 #endif
 
 // WS2812 LED strip definitions
-const uint8_t PIXEL_PIN = 3;
-const uint8_t LED_PIN = 9;  //LED on Moteino. Note that the PCB has an LED space at A3;  //Pin 17
-const uint16_t NUM_PIXELS = 7;
-NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(NUM_PIXELS,PIXEL_PIN);
+const uint8_t PIXEL_PIN = 5;
+const uint8_t LED_PIN = 9;          //LED on Moteino. Note that the PCB has an LED space at A3;  //Pin 17
+const uint8_t TACHOMETER_PIN = 3;
+const uint16_t NUM_PIXELS = 13;
+NeoPixelBus<NeoRgbwFeature, Neo800KbpsMethod> strip(NUM_PIXELS,PIXEL_PIN);
 
+//External Instrument light. 12v through voltage divider to about 3v when instrument lights are on
+const uint8_t INSTRUMENT_LIGHT_PIN = 8;
 
 //SSD1306 OLED definitions
 const int8_t OLED_RESET     = -1; // Reset pin # (or -1 if sharing Arduino reset pin)
@@ -39,52 +76,12 @@ const uint8_t SCREEN_HEIGHT = 32; // OLED display height, in pixels
 const uint8_t SCREEN_ADDRESS = 0x3C; // OLED I2C address
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-// Add font headers from the Adafruit GFX "Fonts" folder.
-// If these headers are not present in your installation adjust the paths/names
-#include <Fonts/FreeMono24pt7b.h>
-#include <Fonts/FreeSerif24pt7b.h>
-#include <Fonts/FreeMonoBold24pt7b.h>
-#include <Fonts/FreeMonoBoldOblique24pt7b.h>
-#include <Fonts/FreeMonoOblique24pt7b.h>
-#include <Fonts/FreeSans24pt7b.h>
-#include <Fonts/FreeSans18pt7b.h>
-#include <Fonts/FreeSans12pt7b.h>
-#include <Fonts/FreeSansBold24pt7b.h>
-#include <Fonts/FreeSansBoldOblique24pt7b.h>
-#include <Fonts/FreeSansOblique24pt7b.h>
-#include <Fonts/FreeSerif24pt7b.h>
-#include <Fonts/FreeSerifBold24pt7b.h>
-#include <Fonts/FreeSerifBoldItalic24pt7b.h>
-#include <Fonts/FreeSerifItalic24pt7b.h>
-
-struct FontInfo { const GFXfont* font; const char* name; };
-const FontInfo g_fonts[] = {
-//    {&FreeMono24pt7b, "FreeMono24pt7b" },
-//    {&FreeSerif24pt7b, "FreeSerif24pt7b" },
-    //{&FreeMonoBold24pt7b, "FreeMonoBold24pt7b" },
-    // {&FreeMonoBoldOblique24pt7b, "FreeMonoBoldOblique24pt7b" },
-    // {&FreeMonoOblique24pt7b, "FreeMonoOblique24pt7b" },
-    // {&FreeSans24pt7b, "FreeSans24pt7b" },
-     {&FreeSans18pt7b, "FreeSans18pt7b" },
-     //{&FreeSans12pt7b, "FreeSans12pt7b" },
-    // {&FreeSansBold24pt7b, "FreeSansBold24pt7b" },
-    // {&FreeSansBoldOblique24pt7b, "FreeSansBoldOblique24pt7b" },
-    // {&FreeSansOblique24pt7b, "FreeSansOblique24pt7b" },
-    // {&FreeSerif24pt7b, "FreeSerif24pt7b" },
-    // {&FreeSerifBold24pt7b, "FreeSerifBold24pt7b" },
-    // {&FreeSerifBoldItalic24pt7b, "FreeSerifBoldItalic24pt7b" },
-    // {&FreeSerifItalic24pt7b, "FreeSerifItalic24pt7b" }
-};
-const size_t FONT_COUNT = sizeof(g_fonts) / sizeof(g_fonts[0]);
-
-
 // APDS9930 proximity sensor
 APDS9930 apds = APDS9930();
 uint16_t proximity_data = 0;
 float ambient_light = 0; // can also be an unsigned long
 uint16_t ch0 = 0;
 uint16_t ch1 = 1;
-
 
 // RF69 network definitions
 RH_RF69 g_rf69;
@@ -107,8 +104,39 @@ typedef enum {
     eLastDisplayMode = eGPSSpeed,
 }  ButtonDisplayMode;
 
+const char* g_displayModeNames[][2] = {
+    "Power", "W x100",
+    "Temp", "Deg C",
+    "Rail", "V /4",
+    "Inv", "W x100",
+    "GPS", "kts",
+};
 
-const uint8_t tachPin = 5; //Pin 5 = OC1A
+const uint8_t MOVING_AVERAGE_COUNT = 5;
+float g_tachometer_movingAverage[MOVING_AVERAGE_COUNT] = {0};
+uint8_t g_movingAverageIndex = 0;
+
+// =============== Font testing routine ====================
+/*
+struct FontInfo { const GFXfont* font; const char* name; };
+const FontInfo g_fonts[] = {
+//    {&FreeMono24pt7b, "FreeMono24pt7b" },
+//    {&FreeSerif24pt7b, "FreeSerif24pt7b" },
+    //{&FreeMonoBold24pt7b, "FreeMonoBold24pt7b" },
+    // {&FreeMonoBoldOblique24pt7b, "FreeMonoBoldOblique24pt7b" },
+    // {&FreeMonoOblique24pt7b, "FreeMonoOblique24pt7b" },
+    // {&FreeSans24pt7b, "FreeSans24pt7b" },
+     {&FreeSans18pt7b, "FreeSans18pt7b" },
+     //{&FreeSans12pt7b, "FreeSans12pt7b" },
+    // {&FreeSansBold24pt7b, "FreeSansBold24pt7b" },
+    // {&FreeSansBoldOblique24pt7b, "FreeSansBoldOblique24pt7b" },
+    // {&FreeSansOblique24pt7b, "FreeSansOblique24pt7b" },
+    // {&FreeSerif24pt7b, "FreeSerif24pt7b" },
+    // {&FreeSerifBold24pt7b, "FreeSerifBold24pt7b" },
+    // {&FreeSerifBoldItalic24pt7b, "FreeSerifBoldItalic24pt7b" },
+    // {&FreeSerifItalic24pt7b, "FreeSerifItalic24pt7b" }
+};
+const size_t FONT_COUNT = sizeof(g_fonts) / sizeof(g_fonts[0]);
 
 
 void setFont(uint8_t fontIndex)
@@ -123,10 +151,11 @@ void setFont(uint8_t fontIndex)
     else
         display.setFont(NULL);  // restore default font
 }
+*/
 
-void setLedStrip(RgbColor inColour, uint8_t intensity = 255 )
+void setLedStrip(RgbwColor inColour, uint8_t intensity = 255 )
 {
-    RgbColor col = RgbColor(inColour.R*intensity/255,inColour.G*intensity/255,inColour.B*intensity/255);
+    RgbwColor col = RgbwColor(inColour.R*intensity/255,inColour.G*intensity/255,inColour.B*intensity/255,0);
 
     for (uint16_t i = 0; i < NUM_PIXELS; ++i)
     {
@@ -145,12 +174,18 @@ bool getProximityOn()
 
 void setTachometer(float value)
 {
-    uint32_t movingAverage[5];
-    uint32_t pwm_frequency =  ( 5000.0 * value + 250.0); // approx
-
-    SetPinFrequencySafe(tachPin, frequency);
-    pwmWrite(tachPin, 254); // ensure pin is High
-
+    g_movingAverageIndex = (g_movingAverageIndex + 1) % MOVING_AVERAGE_COUNT;
+    g_tachometer_movingAverage[g_movingAverageIndex] = value;
+    float sum = 0;
+    for(int i=0;i<MOVING_AVERAGE_COUNT;i++)
+    {
+        sum += g_tachometer_movingAverage[i];
+    }
+    value = sum / MOVING_AVERAGE_COUNT;
+    uint32_t pwm_frequency =  ( 5000.0 * value + 100.0); // approx  250
+//    Serial.print("Tachometer RPM: "); Serial.print(value,3); Serial.print(" PWM frequency: "); Serial.println(pwm_frequency);
+    SetPinFrequencySafe(TACHOMETER_PIN, pwm_frequency);
+    pwmWrite(TACHOMETER_PIN, 254); // ensure pin is High for 254/255 of duty cycle
 }
 
 void setup()
@@ -159,24 +194,26 @@ void setup()
     digitalWrite(LED_PIN, HIGH);
 
     Serial.begin(9600);
-    Serial.println(F("Tachometer starting"));
+    Serial.println("Tachometer starting");
 
     // initialize the OLED object
 
-    if(display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) 
+    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) 
     {
-       Serial.println(F("OLED failed"));
+       Serial.println("OLED begin failed");
     }
     display.setRotation(2); //upside down
-    display.setTextSize(2);
+    display.setTextSize(1);
     display.setFont(&FreeSans12pt7b);
     display.setTextColor(WHITE);
+    display.ssd1306_command(SSD1306_SETCONTRAST);
+    display.ssd1306_command(1);  //set the contrast to the lowest possible. Still seems quite bright in the dark
     display.clearDisplay();
 
     // Initialize APDS-9930 (configure I2C and initial values)
     if ( !apds.init() ) 
     {
-        Serial.println(F("APDS-9930 failed"));
+        Serial.println("APDS-9930 failed");
     }
 
     // Start running the APDS-9930 proximity sensor (no interrupts)
@@ -187,6 +224,8 @@ void setup()
     // Start running the APDS-9930 light sensor (no interrupts)
     apds.setAmbientLightGain(AGAIN_120X);
     apds.enableLightSensor(false);
+
+    pinMode( INSTRUMENT_LIGHT_PIN, INPUT);
 
 
 	if (!g_rf69.init())
@@ -219,6 +258,10 @@ void setup()
     strip.Begin();
     strip.Show();
 
+    //initialise PWM timers for tachometer output
+    //pinMode(TACHOMETER_PIN, OUTPUT);
+    InitTimersSafe();
+
     digitalWrite(LED_PIN, LOW);
 }
 
@@ -226,15 +269,13 @@ void setup()
 
 void loop()
 {
-
+    const unsigned long DISPLAY_UPDATE_INTERVAL_MS = 250;
+    const unsigned long DISPLAYMODE_PERIOD_MS = 2000;
+    static unsigned long lastDisplayUpdateTime = millis();
+    static unsigned long lastDisplayModeChangeTime = millis();
     static ButtonDisplayMode displayMode = eFirstDisplayMode;
-    static RgbColor ledColor = RgbColor(255, 255, 255);
+    static RgbwColor ledColor = RgbwColor(255, 255, 255, 255);
     
-    //Tasks
-    //read APDS9930 proximity and ambient light and update displayNode
-    //recevie data from RF69 nodes
-    //update OLED display
-
 	if (g_rf69.available())
 	{
 		//read the nodeID early to prevent overwriting before the buffer is read
@@ -299,6 +340,7 @@ void loop()
                 displayMode = eFirstDisplayMode;
             else
                 displayMode = (ButtonDisplayMode)((int)displayMode + 1);
+                lastDisplayModeChangeTime = millis();
         }
         else
         {
@@ -307,94 +349,65 @@ void loop()
         proximityOn = !proximityOn;
     }
 
-    display.clearDisplay();
-    display.setCursor(0, 31);
-    switch(displayMode)
-    {
-        case eGPSSpeed:
-            display.print("GPS ");
-            display.print(g_payloadGPS.speed);
-            ledColor = RgbColor(0,0,255); // blue
-            break;
-        case eCurrentTemperatures:
-            display.print((float)(g_payloadTemperature.temperature[0]/100.0),1);
-            ledColor = RgbColor(255,0,0); // red
-            break;
-        case eCurrentPower:
-            display.print((float)(g_payloadPulse.power[2]),1);
-            ledColor = RgbColor(255,255,255); // white
-            break;
-        case eRailVoltage:
-            display.print((float)(g_payloadBattery.voltage[0]/100.0),1);
-            ledColor = RgbColor(0,255,255); // cyan
-            break;
-        case eInverterIn:
-            {
-                unsigned short inverterIn = 0;
-                for(int i=0; i<MAX_INVERTERS;i++)
-                {
-                    inverterIn += g_payloadInverter[i].pvInputPower;
-                }
-                display.print(inverterIn);
-                ledColor = RgbColor(0,255,0); // green
-            }
-            break;
-    }
-    display.display();
 
-    float ambient_light;
-    apds.readAmbientLightLux(ambient_light);
-    setLedStrip(ledColor, map(ambient_light, 0, 1000, 20, 255) ); // adjust brightness based on ambient light
-//    display.print(ambient_light);
-//    display.display();
+    if( lastDisplayUpdateTime + DISPLAY_UPDATE_INTERVAL_MS < millis() )
+    {
+        lastDisplayUpdateTime = millis();
+        
+        uint8_t textOffset = (millis() - lastDisplayModeChangeTime) > DISPLAYMODE_PERIOD_MS;
+        size_t pixOffset = SCREEN_WIDTH/2 - 6*strlen(g_displayModeNames[displayMode][textOffset]);
+        display.clearDisplay();
+        display.setCursor(pixOffset, 26);
+        display.print(g_displayModeNames[displayMode][textOffset]);
+        display.display();
+
+        switch(displayMode)
+        {
+            case eCurrentPower:
+                setTachometer((float)(g_payloadPulse.power[2]/2500.0));
+                ledColor = RgbwColor(255,255,255,255); // white
+                break;
+            case eCurrentTemperatures:
+                setTachometer((float)(g_payloadTemperature.temperature[0]/100.0/25.0));
+                ledColor = RgbwColor(255,0,0,0); // red
+                break;
+            case eRailVoltage:
+                setTachometer((float)(g_payloadBattery.voltage[0]/100.0/100.0));
+                ledColor = RgbwColor(0,255,255,0); // cyan
+                break;
+            case eInverterIn:
+                {
+                    unsigned short inverterIn = 0;
+                    for(int i=0; i<MAX_INVERTERS;i++)
+                    {
+                        inverterIn += g_payloadInverter[i].pvInputPower;
+                    }
+                    setTachometer((float)(inverterIn/2500.0));
+                    ledColor = RgbwColor(0,255,0,0); // green
+                }
+                break;
+            case eGPSSpeed:
+                setTachometer(g_payloadGPS.speed/10.0);
+                ledColor = RgbwColor(0,0,255,0); // blue
+                break;
+        }
+    }
+
+    //do the lighting based on ambient light and whether instrument lights are on
+    uint8_t intensity = 0;
+    if( digitalRead(INSTRUMENT_LIGHT_PIN) == HIGH )
+    {
+        intensity = 2;
+    }
+    else
+    {
+        // Get the intensity of the ambient light. 
+        float ambient_light;
+        apds.readAmbientLightLux(ambient_light);
+        intensity = map(ambient_light, 0, 1000, 3, 255);   //0 turns off when light is very low!
+        //Serial.print(ambient_light);Serial.print(",");Serial.println(intensity);
+    }
+    setLedStrip(ledColor, intensity ); // adjust brightness based on ambient light
 
     return;
-
-    // int fontIndex = 0;
-    // for(uint8_t intensity = 255; intensity > 0 ; intensity -= 51)
-    // {
-    //     if(getProximityOn())
-    //     {
-    //     }
-    //     setFont(fontIndex++%FONT_COUNT);
-    //     display.clearDisplay();
-    //     display.setCursor(0, 31);
-    //     display.print("0125AB");
-    //     display.display();
-    //     Serial.print("Intensity: "); Serial.println(intensity);
-
-    //     setLedStrip(RgbColor(0,255,0), intensity); // green
-    //     delay(200);
-    //     setLedStrip(RgbColor(255,255,0), intensity); // yellow
-    //     delay(200);
-    //     setLedStrip(RgbColor(255,0,0), intensity); // red
-    //     delay(200);
-    //     setLedStrip(RgbColor(0,0,255), intensity); // blue
-    //     delay(200);
-    // }
-
-
-
-    // static unsigned long lastReceivedFromSevCon = millis();
-
-    // if (g_rf69.available())
-    // {
-    //     digitalWrite(LED_PIN, HIGH);
-    //     byte len = RH_RF69_MAX_MESSAGE_LEN;
-    //     uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
-    //     int node_id= -1;
-        
-    //     if (g_rf69.recv(buf, &len))
-    //     {
-    //         node_id = g_rf69.headerId();
-    //     }
-
-    //     if (node_id == SEVCON_CAN_NODE && len == sizeof(PayloadSevCon)) // === SEVCON NODE ====
-    //     {
-    //         g_payloadSevCon = *(PayloadSevCon*)buf;							// get payload data
-    //         EmonSerial::PrintSevConPayload(&g_payloadSevCon);				// print data to serial
-    //         lastReceivedFromSevCon = millis();
-    //     }
-    //     digitalWrite(LED_PIN, LOW);
-    // }
 }
